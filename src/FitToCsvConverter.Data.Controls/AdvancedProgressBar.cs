9f29412869122fc3b7bf63e59366c554587c6e7e
BionicCode <bioniccode@outlook.com>
2026-03-30 23:19:10 +0200

    Add AdvancedProgressBar control and update progress UI
    Introduced AdvancedProgressBar with customizable appearance, progress text, and alignment. Replaced standard ProgressBar in MainWindow with the new control and updated styles. Added supporting converters and enum. Moved project files under src/ in solution structure. Improves progress reporting visuals and flexibility.

---
+++ src/FitToCsvConverter.Data.Controls/AdvancedProgressBar.cs
@@ -0,0 +1,97 @@
+﻿namespace FitToCsvConverter.Controls;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+public class AdvancedProgressBar : ProgressBar
+{
+    public double CornerRadius
+    {
+        get => (double)GetValue(CornerRadiusProperty);
+        set => SetValue(CornerRadiusProperty, value);
+    }
+
+    public static readonly DependencyProperty CornerRadiusProperty =
+        DependencyProperty.Register(
+            nameof(CornerRadius),
+            typeof(double),
+            typeof(AdvancedProgressBar),
+            new FrameworkPropertyMetadata(4.0, FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+    public double ProgressTextSize
+    {
+        get => (double)GetValue(ProgressTextSizeProperty);
+        set => SetValue(ProgressTextSizeProperty, value);
+    }
+
+    public static readonly DependencyProperty ProgressTextSizeProperty =
+        DependencyProperty.Register(
+            nameof(ProgressTextSize),
+            typeof(double),
+            typeof(AdvancedProgressBar),
+            new FrameworkPropertyMetadata(12.0, FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+    public Brush ProgressTextBrush
+    {
+        get => (Brush)GetValue(ProgressTextBrushProperty);
+        set => SetValue(ProgressTextBrushProperty, value);
+    }
+
+    public static readonly DependencyProperty ProgressTextBrushProperty =
+        DependencyProperty.Register(
+            nameof(ProgressTextBrush),
+            typeof(Brush),
+            typeof(AdvancedProgressBar),
+            new FrameworkPropertyMetadata(Brushes.Black));
+
+    public double ProgressPercentage
+    {
+        get => (double)GetValue(ProgressPercentageProperty);
+        set => SetValue(ProgressPercentageProperty, value);
+    }
+
+    public static readonly DependencyProperty ProgressPercentageProperty =
+        DependencyProperty.Register(
+            nameof(ProgressPercentage),
+            typeof(double),
+            typeof(AdvancedProgressBar),
+            new FrameworkPropertyMetadata(0.0));
+
+    public Thickness ProgressTextMargin
+    {
+        get => (Thickness)GetValue(ProgressTextMarginProperty);
+        set => SetValue(ProgressTextMarginProperty, value);
+    }
+
+    public static readonly DependencyProperty ProgressTextMarginProperty =
+        DependencyProperty.Register(
+            nameof(ProgressTextMargin),
+            typeof(Thickness),
+            typeof(AdvancedProgressBar),
+            new FrameworkPropertyMetadata(new Thickness(4)));
+
+    public ProgressTextAlignment ProgressTextAlignment
+    {
+        get => (ProgressTextAlignment)GetValue(ProgressTextAlignmentProperty);
+        set => SetValue(ProgressTextAlignmentProperty, value);
+    }
+
+    public static readonly DependencyProperty ProgressTextAlignmentProperty =
+        DependencyProperty.Register(
+            nameof(ProgressTextAlignment),
+            typeof(ProgressTextAlignment),
+            typeof(AdvancedProgressBar),
+            new FrameworkPropertyMetadata(ProgressTextAlignment.Center));
+
+    protected override void OnValueChanged(double oldValue, double newValue)
+    {
+        base.OnValueChanged(oldValue, newValue);
+        if (oldValue != newValue)
+        {
+            ProgressPercentage = Maximum != 0 && Maximum > Minimum
+            ? (newValue - Minimum) / (Maximum - Minimum) * 100
+            : 0;
+        }
+    }
+}
---
+++ src/FitToCsvConverter.Data.Controls/ProgressTextAlignmentToGridRowConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace FitToCsvConverter.Controls;
+
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+public class ProgressTextAlignmentToGridRowConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is ProgressTextAlignment alignment)
+        {
+            return alignment switch
+            {
+                ProgressTextAlignment.Left => 1,
+                ProgressTextAlignment.Center => 1,
+                ProgressTextAlignment.Right => 1,
+                ProgressTextAlignment.TopCenter => 0,
+                ProgressTextAlignment.BottomCenter => 2,
+                _ => 1
+            };
+        }
+
+        return HorizontalAlignment.Center;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotSupportedException();
+}
